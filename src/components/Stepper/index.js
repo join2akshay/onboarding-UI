4ad0e85737482b75e3a3ca09340d398e0588c552
akshay <akshay>
2022-10-16 19:59:53 +0530

    fix warning

--- src/components/Stepper/index.js
+++ src/components/Stepper/index.js
@@ -7,14 +7,14 @@ export default function Stepper({totalStep,currentStep}) {
   return ( 
         <div className="mdl-stepper-horizontal-alternative">
       {
-        arr.map((item,i)=><>
-        <div className={`mdl-stepper-step ${currentStep>=i ? 'active-step' : ''}`}>
+        arr.map((item,i)=>
+        <div key={i}className={`mdl-stepper-step ${currentStep>=i ? 'active-step' : ''}`}>
         <div className="mdl-stepper-circle"><span>{i+1}</span></div>
        
         <div className={`mdl-stepper-bar-left ${currentStep>=i ? 'active-step-left' : ''}`}></div>
         <div className={`mdl-stepper-bar-right ${currentStep>=i ? 'active-step-right' : ''}`}></div>
       </div>
-        </>)
+        )
       }
     </div>
 
